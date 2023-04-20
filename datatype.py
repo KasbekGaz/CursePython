9247493fa1f393f9db474a3d92e161c4d594cea5
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-04-20 17:11:21 -0600

    Primer commit

---
+++ datatype.py
@@ -0,0 +1,31 @@
+# integer
+print(type(30)) #para ver el tipo de dato #entero
+# float
+print(type(30.5)) #decimal
+#boolean tipo de dato logico
+print(type(true))
+print(type(false))
+# Lists
+[10, 20, 30, 40, 50]
+['saludos', 'hola conschtumare']
+[10, 'hello', 10.2]
+[] #lista vacia
+print(type(['elemento',12]))
+# Tuplas similar a las listas pero no se pueden cambiar los datos.
+(10, 20, 30, 55)
+()#tupla vacia
+print(type((10, 20, 30)))
+# diccionarios: agrupar distintos datos de la misma entidad
+#siempre como CLAVE:VALOR
+{     #ejemplo de una entidad PERSONA
+	 "nombre":"Ryan",
+	"apellido1":"papadopolus",
+	"apellido2":"eterminator",
+}
+# Aqui nos marca un error de algo equisde pero no se de que
+#print(type({"pais": "mexico", "latitud": 123.123.123, "longitud": 123.456.78.9}))
+
+
+#Tipos de datos que no estan definidos
+None
+print(type(None))
