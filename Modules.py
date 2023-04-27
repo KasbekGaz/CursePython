c3a985894ba19cfb29047f7b57131d326dd2edcd
KasbekGaz <oswa.laza.flores@hotmail.com>
2023-04-27 11:47:13 -0600

    Finalizando Curso

---
+++ Modules.py
@@ -0,0 +1,10 @@
+#aqui vamos usar uso de un modulo de python
+import datetime
+from fmath import sumar, resta
+
+
+print(datetime.date.today()) #saber la fecha de hoy
+print(datetime.timedelta(minutes=90)) #para saber tiempo en minutos convertir horas y minutos
+
+sumar(12,12)
+resta(9,5)
---
+++ fmath.py
@@ -0,0 +1,8 @@
+
+#aqui hacemos una fucion donde sume dos numeros
+def sumar(n1, n2):
+    print(n1 + n2)
+##aqui una que las reste
+def resta(n1,n2):
+    print(n1-n2)
+    
